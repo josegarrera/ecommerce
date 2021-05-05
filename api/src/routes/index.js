733dacd42e3fad1942172d7e67752874dc25fa64
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-05-05 14:12:52 -0300

    Adding model, routes and controller of Categories

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const products = require("./products.js");
+const categories = require("./categories.js")
 
 router.use("/products", products);
+router.use("/categories", categories);
 
 module.exports = router;
