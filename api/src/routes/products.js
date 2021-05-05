94310803ebf132b1151ca2239b82e9ea8407cf37
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-05-05 15:02:31 -0300

    Adding getProducts controller

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,7 +1,8 @@
-const {createProduct} = require("../controllers/products.js");
+const {createProduct, getProducts} = require("../controllers/products.js");
 const { Router } = require("express");
 const router = Router();
 
 router.post("/", createProduct);
+router.get("/", getProducts)
 
 module.exports = router;
