689f9632b45f03a221b270cca03cea2bccdf4eeb
Alonso Diaz <superalonso123@hotmail.com>
2021-05-05 21:45:19 -0300

    add models

--- api/src/controllers/categories.js
+++ api/src/controllers/categories.js
@@ -1,35 +1,50 @@
-const {Categories, Products} = require("../models/index.js");
+const {Categories, Products} = require('../models/index.js');
 
-function createCategories (req, res) {
-if(!req.body || !req.body.categories) return res.status(400).send({type: "Bad request.", error: "The fields are empty."});
-const idProducts = req.body.products
-const categories = req.body.categories
-const specProducts = idProducts.map(item => Products.exists({_id: item}));
-const validProducts = [];
 
-Promise.all(specProducts)
-.then(data => {
-    const filterProducts = idProducts.filter((item, i) => data[i] === true && validProducts.push(item));
-    const categoriesCreated = categories.map(item => 
+function getAllCategories(req, res) {
+	Categories.find({}, 'name')
+		.exec()
+		.then((data) => res.send(data))
+		.catch((error) =>
+			res.status(500).send({type: 'Internal Server Error', error: error})
+		);
+}
 
-  Categories.findOrCreate({name: item}));
-    console.log("antes promise all", categoriesCreated)
-    return Promise.all(categoriesCreated); 
-})
-.then(data => {
-    data.map(item => item.doc.products = validProducts)
-    return Promise.all(data.map(item => item.doc.save()))
-})
-.then(data => {
-    res.send(data);
-})
+function createCategories(req, res) {
+	if (!req.body || !req.body.categories)
+		return res
+			.status(400)
+			.send({type: 'Bad request.', error: 'The fields are empty.'});
+	const idProducts = req.body.products;
+	const categories = req.body.categories;
+	const specProducts = idProducts.map((item) => Products.exists({_id: item}));
+	const validProducts = [];
 
-.catch(error => 
-    res.status(500).send({type: "Internal Server Error", error: error})
-) 
- 
+	Promise.all(specProducts)
+		.then((data) => {
+			const filterProducts = idProducts.filter(
+				(item, i) => data[i] === true && validProducts.push(item)
+			);
+			const categoriesCreated = categories.map((item) =>
+				Categories.findOrCreate({name: item})
+			);
+			console.log('antes promise all', categoriesCreated);
+			return Promise.all(categoriesCreated);
+		})
+		.then((data) => {
+			data.map((item) => (item.doc.products = validProducts));
+			return Promise.all(data.map((item) => item.doc.save()));
+		})
+		.then((data) => {
+			res.send(data);
+		})
+
+		.catch((error) =>
+			res.status(500).send({type: 'Internal Server Error', error: error})
+		);
 }
 
 module.exports = {
-    createCategories
-}
+	createCategories,
+	getAllCategories,
+};
--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,7 +1,11 @@
-const {createCategories} = require("../controllers/categories.js");
-const { Router } = require("express");
+const {
+	createCategories,
+	getAllCategories,
+} = require('../controllers/categories.js');
+const {Router} = require('express');
 const router = Router();
 
-router.post("/", createCategories);
+router.post('/', createCategories);
+router.get('/', getAllCategories);
 
 module.exports = router;
