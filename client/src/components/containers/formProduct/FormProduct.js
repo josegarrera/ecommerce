3a3996d2159a37c1ba6f3dd6406341dc8c60278b
Gregorio Martocci <gregoriomartocci@gmail.com>
2021-05-08 18:24:03 -0300

    arrancando la validacion del form

--- client/src/components/containers/formProduct/FormProduct.js
+++ client/src/components/containers/formProduct/FormProduct.js
@@ -6,9 +6,55 @@ import Checkbox from "../checkbox";
 import Dropdown from "../dropdown";
 // import { getCategories } from "../../../redux/actions/index.js";
 
+export function validate(product, allProducts) {
+  let errors = {};
+
+  if (!product.name) {
+    errors.name = "Campo requerido.";
+  }
+  if (!product.brand) {
+    errors.weight = "Campo requerido.";
+  }
+  if (!product.description) {
+    errors.weight = "Campo requerido.";
+  }
+  if (!product.price) {
+    errors.weight = "Campo requerido.";
+  }
+
+  if (product.price && !/[0-9-]+$/.test(product.price)) {
+    errors.price = "Sólo un rango de números, ejemplo: 5 - 8";
+  }
+
+  if (product.name && !/^[A-Za-z\s]+$/g.test(product.name)) {
+    errors.name = "Sólo palabras sin tilde.";
+  }
+
+  // if (
+  //   product.name &&
+  //   allProducts.length &&
+  //   allProducts.find(
+  //     (element) =>
+  //       element.name.toLowerCase().trim() === product.name.toLowerCase().trim()
+  //   )
+  // ) {
+  //   errors.name = "La raza ya existe.";
+  // }
+
+  // if (
+  //   product.imageUrl &&
+  //   (!/image\/jpeg|png/.test(product.imageUrl[0]) || product.imageUrl > 5242880)
+  // ) {
+  //   errors.file = "Sólo imágenes .png y .jpeg, menores a 5.24 MB.";
+  // }
+
+  return errors;
+}
+
 const FormProduct = () => {
   const [variantSelected, setVariants] = useState([]);
   const [categorySelected, setCategories] = useState([]);
+  const [errors, setErrors] = useState({});
 
   //   {
   //     "price" : {"currency": "$", "value": 249999},
