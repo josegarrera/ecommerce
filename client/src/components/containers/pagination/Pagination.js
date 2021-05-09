8a3fe26ef8a74fde728f61bb0510b4a4cd8e21a4
Bruno Paveglio <pavegliobruno@gmail.com>
2021-05-09 18:50:18 -0300

    Cambios de Bruno

--- client/src/components/containers/pagination/Pagination.js
+++ client/src/components/containers/pagination/Pagination.js
@@ -1,17 +1,24 @@
-import React from "react";
-import DivPag from "./styled";
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import DivPag from './styled';
+import {getProductsQuery} from '../../../redux/actions/index.js';
 
-const Pagination = ({ handleNextPage, handlePrevPage }) => {
-  return (
-    <DivPag>
-      <button className="btn__prev" onClick={handlePrevPage}>
-        Prev
-      </button>
-      <button className="btn__next" onClick={handleNextPage}>
-        Next
-      </button>
-    </DivPag>
-  );
+const Pagination = ({pages, actualizar}) => {
+	const dispatch = useDispatch();
+
+	function actualizar(page) {
+		console.log('estoy aca', page);
+		dispatch(getProductsQuery(page));
+	}
+
+	return (
+		<DivPag>
+			{pages &&
+				pages.map((page) => (
+					<button className={page} onClick={() => actualizar(page)}></button>
+				))}
+		</DivPag>
+	);
 };
 
 export default Pagination;
