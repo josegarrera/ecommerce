f8994c86d4da11a1f253666c3508b639696a1b73
Bruno Paveglio <pavegliobruno@gmail.com>
2021-05-10 11:53:35 -0300

    Search working

---
+++ client/src/components/containers/productsList/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Products_List_Style = styled.div`
+	.cards__container {
+		display: flex;
+		padding: 0 2rem;
+		flex-wrap: wrap;
+		width: 100%;
+	}
+`;
+
+export default Products_List_Style;
