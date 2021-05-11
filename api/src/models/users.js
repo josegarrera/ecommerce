302fcabd994a82020b55d0d027ef1985a7f87519
josegarrera <josegarrera@josegarrera-IdeaPad-3-15IIL05>
2021-05-11 11:21:38 -0300

    Users model modification for authentication

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UsersSchema = new Schema({
 	username: {
 		type: String,
 		required: [true, 'Username is required'],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
 	},
 	firstName: String,
 	lastName: String,
@@ -17,6 +23,7 @@ const UsersSchema = new Schema({
 			},
 			message: (props) => `${props.value} is not a valid email!`,
 		},
+		unique: true,
 	},
 	type: {
 		type: String,
@@ -33,5 +40,19 @@ const UsersSchema = new Schema({
 		},
 	],
 });
+
+// hook to execute before save any new user
+UsersSchema.pre('save', async function (next) {
+	const hash = await bcrypt.hash(this.password, 10);
+	this.password = hash;
+	next();
+});
+
+// method to compare password from input with password from DB
+UsersSchema.methods.isValidPassword = async function (password) {
+	const user = this;
+	const compare = await bcrypt.compare(password, user.password);
+	return compare;
+};
 
 module.exports = mongoose.model('users', UsersSchema);
