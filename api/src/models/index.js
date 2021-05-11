c032bb2a6cae6aad60a67479ee6080b62bb0d53a
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-05-11 20:36:25 -0300

    GetProducts by concatened filters

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,9 +1,11 @@
 const Products = require('../models/products.js');
 const Categories = require('../models/categories.js');
 const Brands = require('../models/brands.js');
+const Currencies = require('../models/currencies.js');
 
 module.exports = {
 	Products,
 	Categories,
 	Brands,
+	Currencies,
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,11 @@ const router = Router();
 const products = require('./products.js');
 const categories = require('./categories.js');
 const brands = require('./brands.js');
+const currencies = require('./currencies.js');
 
 router.use('/products', products);
 router.use('/categories', categories);
 router.use('/brands', brands);
+router.use('/currencies', currencies);
 
 module.exports = router;
