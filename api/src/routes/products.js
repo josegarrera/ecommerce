f98c78b0d775fb73b0be60c83aa9c175573cd6ce
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-05-12 15:37:53 -0300

    Adding product update path and fixing currencies path

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -2,6 +2,7 @@ const {
 	createProduct,
 	getProducts,
 	getProductsDetail,
+	updateProduct,
 } = require('../controllers/products.js');
 const {Router} = require('express');
 const router = Router();
@@ -10,5 +11,5 @@ const upload = require('../middlewares/uploads.js');
 router.post('/', upload.array('images'), createProduct);
 router.get('/', getProducts);
 router.get('/:id', getProductsDetail);
-
+router.put('/:id', updateProduct);
 module.exports = router;
