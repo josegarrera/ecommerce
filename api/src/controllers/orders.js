c49417ff7eadf5452765a5337a172ba8563d265f
Pedro Contreras <pedrocontreras182@gmail.com>
2021-05-12 14:26:19 -0300

    getUserOrders controller

--- api/src/controllers/orders.js
+++ api/src/controllers/orders.js
@@ -9,7 +9,10 @@ async function getUserOrder(req, res) {
 		if (userExists) {
 			let orderExist = await Orders.exists({users: userId, state: 1});
 			if (orderExist) {
-				let order = await Orders.findOne({users: userId, state: 1});
+				let order = await Orders.findOne({users: userId, state: 1})
+					.populate('users', {email: 1, _id: 1})
+					.populate('items.product', {name: 1, price: 1, _id: 1})
+					.exec();
 				console.log(order);
 				return res.send(order);
 			}
@@ -17,30 +20,33 @@ async function getUserOrder(req, res) {
 			order.save();
 			res.send(order);
 		} else {
-			res.send({error: "user doesn't exist"});
+			res.status(400).send({type: 'Bad request', error: 'user does not exist'});
 		}
-	} catch (err) {
-		console.log(err);
+	} catch (error) {
+		res.status(500).send({type: 'Internal server error.', error: error});
 	}
 }
 
 async function addProduct(req, res) {
 	const {userId, products} = req.body;
 	try {
-		let existe = await Orders.exists({users: userId, state: 1});
-		if (existe) {
-			let order = await Orders.findOne({users: userId, state: 1});
-			console.log(order);
-			console.log(products);
+		let userExists = await Users.exists({_id: userId});
+		if (userExists) {
+			let existe = await Orders.exists({users: userId, state: 1});
+			if (existe) {
+				let order = await Orders.findOne({users: userId, state: 1});
+				console.log(order);
+				console.log(products);
 
-			order.products = order.products.concat(products);
-			console.log(order.products);
-			await order.save();
-			return res.send(order);
+				order.products = order.products.concat(products);
+				console.log(order.products);
+				await order.save();
+				return res.send(order);
+			}
+			let order = await new Orders({users: userId, products});
+			order.save();
+			res.send(order);
 		}
-		let order = await new Orders({users: userId, products});
-		order.save();
-		res.send(order);
 	} catch (err) {
 		console.log(err);
 	}
