585ce1cf07ca37fee169f05d96ed3007d4c00a7c
josegarrera <josegarrera@josegarrera-IdeaPad-3-15IIL05>
2021-05-12 15:52:39 -0300

    getAllOrders controller

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -2,10 +2,12 @@ const Products = require('../models/products.js');
 const Categories = require('../models/categories.js');
 const Brands = require('../models/brands.js');
 const Currencies = require('../models/currencies.js');
+const Orders = require('../models/orders.js');
 
 module.exports = {
 	Products,
 	Categories,
 	Brands,
 	Currencies,
+	Orders,
 };
--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,10 +1,14 @@
 const {Router} = require('express');
 const router = Router();
-const Orders = require('../models/orders');
-const {getUserOrder, addProduct} = require('../controllers/orders');
+const {
+	getUserOrder,
+	addProduct,
+	getAllOrders,
+} = require('../controllers/orders');
 
 router.post('/', addProduct);
 
 router.get('/', getUserOrder);
+router.get('/', getAllOrders);
 
 module.exports = router;
