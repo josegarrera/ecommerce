c57cbe22103bd8548155dff43028066cf9ee282a
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-05-14 20:19:36 -0300

    Add Products to cart and Local Storage

--- client/src/components/presentationals/cardProduct/CardProduct.js
+++ client/src/components/presentationals/cardProduct/CardProduct.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import DivCrdProd from './styled';
-import {Link} from 'react-router-dom';
-import {FaShoppingCart} from 'react-icons/fa';
-import {AiOutlineHeart} from 'react-icons/ai';
+import React from "react";
+import DivCrdProd from "./styled";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addCartProduct } from "../../../redux/actions";
 
-const CardProduct = ({name, imageUrl, price, _id}) => {
-	const handleAddCart = () => {
-		//add to cart
-	};
-	const handleAddFav = () => {
-		//add to fav
-	};
+const CardProduct = ({ name, imageUrl, price, _id }) => {
+  const dispatch = useDispatch();
 
-	return (
-		<DivCrdProd>
-			<div className='cnt__image'>
-				<Link to={`/products/id/${_id}`}>
-					<img
-						className='img__card'
-						src={imageUrl[0]}
-						alt='imagen de producto'
-					/>
-				</Link>
-			</div>
-			<div className='cnt_info'>
-				<Link to={`/products/id/${_id}`}>
-					<h5>{name}</h5>
-				</Link>
-				<h6>{price.currency + price.value}</h6>
-				<button className='btn__fav' onClick={handleAddFav}>
-					<AiOutlineHeart />
-				</button>
-				<button className='btn__cart' onClick={handleAddCart}>
-					<FaShoppingCart />
-				</button>
-			</div>
-		</DivCrdProd>
-	);
+  const handleAddCart = () => {
+    //add to cart
+    dispatch(addCartProduct(_id));
+  };
+  const handleAddFav = () => {
+    //add to fav
+  };
+
+  return (
+    <DivCrdProd>
+      <div className="cnt__image">
+        <Link to={`/products/id/${_id}`}>
+          <img
+            className="img__card"
+            src={imageUrl[0]}
+            alt="imagen de producto"
+          />
+        </Link>
+      </div>
+      <div className="cnt_info">
+        <Link to={`/products/id/${_id}`}>
+          <h5>{name}</h5>
+        </Link>
+        <h6>{price.currency + price.value}</h6>
+        <button className="btn__fav" onClick={handleAddFav}>
+          <AiOutlineHeart />
+        </button>
+        <button className="btn__cart" onClick={handleAddCart}>
+          <FaShoppingCart />
+        </button>
+      </div>
+    </DivCrdProd>
+  );
 };
 
 export default CardProduct;
