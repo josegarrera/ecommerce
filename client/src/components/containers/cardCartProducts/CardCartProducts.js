8c9908fb9a2dae252b564c138b04bca603e9f57a
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-05-13 21:05:30 -0300

    preparing Cart and CardCart

--- client/src/components/containers/cardCartProducts/CardCartProducts.js
+++ client/src/components/containers/cardCartProducts/CardCartProducts.js
@@ -1,7 +1,8 @@
 import React from "react";
 import DivCard from "./styled";
 
-const CardCartProducts = () => {
+const CardCartProducts = (props) => {
+  console.log(props.product.product._id);
   return (
     <DivCard>
       <div>CardCartProducts</div>
