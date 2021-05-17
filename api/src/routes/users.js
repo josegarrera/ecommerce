3e7940ddad1c774015274ff1d5deb180020122e5
josegarrera <josegarrera@josegarrera-IdeaPad-3-15IIL05>
2021-05-17 14:03:19 -0300

    all users route

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,7 +1,8 @@
-const {deleteUser} = require('../controllers/users.js');
+const {deleteUser, getAllUsers} = require('../controllers/users.js');
 const {Router} = require('express');
 const router = Router();
 
+router.get('/', getAllUsers);
 router.delete('/', deleteUser);
 
 module.exports = router;
