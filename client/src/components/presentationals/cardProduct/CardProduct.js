ba5e7978a3af18c22a5fd984209fa77907df571f
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-05-20 11:25:35 -0300

    add Wishlist to Local Storage

--- client/src/components/presentationals/cardProduct/CardProduct.js
+++ client/src/components/presentationals/cardProduct/CardProduct.js
@@ -1,66 +1,86 @@
-import React, {useState} from 'react';
-import DivCrdProd, {StyledLoder} from './styled';
+import React, { useState, useEffect } from "react";
+import DivCrdProd, { StyledLoder } from "./styled";
 
-import {Link} from 'react-router-dom';
-import {FaShoppingCart} from 'react-icons/fa';
-import {AiOutlineHeart} from 'react-icons/ai';
-import {useDispatch} from 'react-redux';
-import {addCartProduct} from '../../../redux/actions';
-import cardLoder from '../../../utils/cardLoader'
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addCartProduct,
+  addFavProduct,
+  getProducts,
+  removeFavProduct,
+} from "../../../redux/actions";
+import cardLoder from "../../../utils/cardLoader";
 
-const CardProduct = ({name, imageUrl, price, _id, loading}) => {
-	const dispatch = useDispatch();
+const CardProduct = ({ name, imageUrl, price, _id, loading }) => {
+  const wishlist = useSelector((state) => state.wishlist);
+  const dispatch = useDispatch();
+  const [fav, setFav] = useState(true);
+  const [Load, setLoad] = useState("loading");
 
-	const [Load, setLoad] = useState('loading');
+  const handleAddCart = () => {
+    //add to cart
+    dispatch(addCartProduct(_id));
+  };
 
-	const handleAddCart = () => {
-		//add to cart
-		dispatch(addCartProduct(_id));
-	};
-	const handleAddFav = () => {
-		//add to fav
-	};
+  const handleAddFav = () => {
+    //add to fav
+    setFav(!fav);
+    dispatch(addFavProduct(_id));
+    //addToFav action=>reducer=>localStorage
+  };
 
+  const handleRemoveFav = () => {
+    //add to fav
+    setFav(!fav);
+    dispatch(removeFavProduct(_id));
+    //removeToFav action=>reducer=>localStorage
+  };
 
+  const handleImageLoaded = () => {
+    setLoad("loaded");
+  };
 
-	const handleImageLoaded = () => {
-		setLoad('loaded');
-	};
+  if (Load === "loading" && loading === true) {
+    return <StyledLoder>{cardLoder()}</StyledLoder>;
+  } else {
+    return (
+      <DivCrdProd>
+        <div className="cnt__image">
+          <Link to={`/products/id/${_id}`}>
+            <img
+              className="img__card"
+              src={imageUrl[0]}
+              alt="imagen de producto"
+              onLoad={handleImageLoaded}
+            />
+          </Link>
+        </div>
+        <div className="cnt_info">
+          <div className="row">
+            <Link to={`/products/id/${_id}`}>
+              <h5>{name}</h5>
+            </Link>
+            <h6>{price.currency + price.value}</h6>
+            {fav ? (
+              <button className="btn__fav" onClick={handleAddFav}>
+                <AiOutlineHeart />
+              </button>
+            ) : (
+              <button className="btn__fav" onClick={handleRemoveFav}>
+                <AiFillHeart />
+              </button>
+            )}
+          </div>
 
-	if (Load === 'loading' && loading === true) {
-		return <StyledLoder>{cardLoder()}</StyledLoder>;
-	} else {
-		return (
-			<DivCrdProd>
-				<div className='cnt__image'>
-					<Link to={`/products/id/${_id}`}>
-						<img
-							className='img__card'
-							src={imageUrl[0]}
-							alt='imagen de producto'
-							onLoad={handleImageLoaded}
-						/>
-					</Link>
-				</div>
-				<div className='cnt_info'>
-					<div className='row'>
-						<Link to={`/products/id/${_id}`}>
-							<h5>{name}</h5>
-						</Link>
-						<h6>{price.currency + price.value}</h6>
-						<button className='btn__fav' onClick={handleAddFav}>
-							<AiOutlineHeart />
-						</button>
-					</div>
-
-					<button className='btn__cart' onClick={handleAddCart}>
-						<FaShoppingCart />
-					</button>
-				</div>
-			</DivCrdProd>
-		);
-	}
-
+          <button className="btn__cart" onClick={handleAddCart}>
+            <FaShoppingCart />
+          </button>
+        </div>
+      </DivCrdProd>
+    );
+  }
 };
 
 export default CardProduct;
