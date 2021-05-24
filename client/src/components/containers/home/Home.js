7de43bb6a675e35493baecb5410d05adde84104b
Bruno Paveglio <pavegliobruno@gmail.com>
2021-05-23 23:14:09 -0300

    Some changes in home

--- client/src/components/containers/home/Home.js
+++ client/src/components/containers/home/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import HomeStyle from './styled';
 
 const Home = () => {
-	return <div>Estas en el home por que no estas loggeado</div>;
+	return (
+		<HomeStyle>
+			<div className='carrousel'>Carrousel</div>
+			<div className='offers'>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+			</div>
+			<div className='carrousel'>Carrousel</div>
+			<div className='offers'>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+				<div className='offer'>Offers</div>
+			</div>
+		</HomeStyle>
+	);
 };
 
 export default Home;
---
+++ client/src/components/containers/home/styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+const HomeStyle = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	a {
+		text-decoration: none;
+		color: #a1a3a6;
+	}
+
+	.carrousel {
+		background-color: #e0e0e0;
+		width: 80%;
+		height: 400px;
+		margin: 50px 0px;
+	}
+
+	.offers {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 50px 50px;
+
+		.offer {
+			width: 224px;
+			height: 430px;
+			border: 1px solid #e0e0e0;
+			border-radius: 20px;
+			margin: 0px 50px;
+			background-color: #e0e0e0;
+		}
+	}
+`;
+
+export default HomeStyle;
