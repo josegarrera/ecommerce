8ffea73eddd35d903b19893a82a7a54dcd9d9184
Gregorio Martocci <gregoriomartocci@gmail.com>
2021-05-27 17:28:14 -0300

    mergeando con lo ultimo

--- client/src/components/containers/carousel/styled.js
+++ client/src/components/containers/carousel/styled.js
@@ -1,31 +1,82 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CarouselStyle = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	align-content: center;
-	width: 100%;
+  width: 100%;
 
-	.slider {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		align-content: center;
-		max-width: 90%;
-		max-height: 90%;
-		object-fit: contain;
-		padding-top: 30px;
+  .slider {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    max-width: 90%;
+    max-height: 90%;
+    object-fit: contain;
+    padding-top: 30px;
 
-		.div__img {
-			.slide__img {
-				margin: auto auto;
-				max-width: 100%;
-				max-height: 100%;
-				object-fit: contain;
-			}
-		}
-	}
+    .div__img {
+      .slide__img {
+        margin: auto auto;
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+      }
+    }
+  }
+
+  .slick-dots {
+    bottom: 2rem;
+  }
+
+  .slick-dots li button {
+    margin: 0 1rem;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 2rem;
+    border: 2px solid #eeeeee;
+  }
+
+  .slick-dots li button {
+    margin: 0 1rem;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 2rem;
+    border: 2px solid #eeeeee;
+  }
+
+  .slick-dots .slick-active button {
+    background-color: #eeeeee;
+  }
+
+  .slick-dots li button::before {
+    display: none;
+  }
+
+  .slick-arrow.slick-prev,
+  .slick-arrow.slick-next {
+    width: 4rem;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 10rem;
+    color: #ffffff;
+    opacity: 0.5;
+  }
+
+  .slick-prev {
+    left: 0.5rem;
+    z-index: 1;
+  }
+
+  .slick-next {
+    right: 0.5rem;
+    z-index: 1;
+  }
+
+  .slick-prev::before,
+  .slick-next::before {
+    display: none;
+  }
 `;
 
 export default CarouselStyle;
