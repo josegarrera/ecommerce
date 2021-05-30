b2aa889b3839f40cd36b4f8c7efa4dfd137d96b9
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-05-30 11:11:53 -0300

    end Verify

--- api/src/routes/favourites.js
+++ api/src/routes/favourites.js
@@ -6,8 +6,10 @@ const {
 	deleteFavourite,
 } = require('../controllers/favourites');
 
-router.get('/', getUserFavourites); //SI JWT
-router.post('/', addFavourite); //SI JWT
-router.delete('/', deleteFavourite); //SI JWT
+const {verifyToken} = require('../middlewares/authJwt');
+
+router.get('/', [verifyToken], getUserFavourites); //SI JWT
+router.post('/', [verifyToken], addFavourite); //SI JWT
+router.delete('/', [verifyToken], deleteFavourite); //SI JWT
 
 module.exports = router;
