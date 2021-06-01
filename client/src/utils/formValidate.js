ed3c5cea02790096fa7d60f1d145ed0621254498
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-06-01 18:45:18 -0300

    Modifing formValidate

--- client/src/utils/formValidate.js
+++ client/src/utils/formValidate.js
@@ -28,31 +28,6 @@ export default function validate(product, allProducts) {
 		return errors;
 	}
 
-	if (!product.images.length && !product.imagesUrl.length) {
-		errors.images = 'You must add at least one image of the product.';
-		return errors;
-	}
-
-	if (product.images.length) {
-		const filesFilter = product.images.map(
-			(file) => !/image\/jpeg|png/.test(file.type) || file.size > 5242880
-		);
-		if (filesFilter.includes(true)) {
-			errors.files = 'Only .png and .jpeg images, smaller than 5.24 MB.';
-			return errors;
-		}
-	}
-
-	if (product.imageUrl) {
-		if (
-			!/(https?:\/\/)?([\w-])+\.{1}([a-zA-Z]{2,63})([/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/g.test(
-				product.imageUrl
-			)
-		) {
-			errors.url = 'URL invalid.';
-			return errors;
-		}
-	}
 	if (!product.brands.length) {
 		errors.brands = 'You must add at least one brand.';
 		return errors;
@@ -61,14 +36,18 @@ export default function validate(product, allProducts) {
 		errors.categories = 'You must add at least one brand.';
 		return errors;
 	}
+	if (!product.variant.stock) {
+		errors.variants = 'You must add at least one variant.';
+		return errors;
+	}
 
 	if (Object.keys(product.variant).length) {
 		if (!product.variant.stock) {
-			errors.variantStock = 'You must add the stock of the variant.';
+			errors.variants = 'You must add the stock of the variant.';
 			return errors;
 		}
 		if (!product.variant.imageUrl && !product.variant.imageFile.name) {
-			errors.variantImage = 'You must add an image of the variant.';
+			errors.variants = 'You must add an image of the variant.';
 			return errors;
 		} else if (
 			product.variant.imageUrl &&
@@ -76,14 +55,14 @@ export default function validate(product, allProducts) {
 				product.variant.imageUrl
 			)
 		) {
-			errors.variantImage = 'Invalid URL.';
+			errors.variants = 'Invalid URL.';
 			return errors;
 		} else if (
 			product.variant.imageFile.name &&
 			(!/image\/jpeg|png/.test(product.variant.imageFile.type) ||
 				product.variant.imageFile.size > 5242880)
 		) {
-			errors.variantImage = 'Only .png and .jpeg images, smaller than 5.24 MB.';
+			errors.variants = 'Only .png and .jpeg images, smaller than 5.24 MB.';
 		}
 	}
 
