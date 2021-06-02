a44a25e7e6f608555aa37d645930c608d556e1f0
Gregorio Martocci <gregoriomartocci@gmail.com>
2021-06-02 01:10:19 -0300

    resizing

--- client/src/components/containers/productsList/ProductList.js
+++ client/src/components/containers/productsList/ProductList.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import CardProduct from '../../presentationals/cardProduct/CardProduct';
-import PRODUCTS_LIST_STYLE from './styled';
+import React from "react";
+import CardProduct from "../../presentationals/cardProduct/CardProduct";
+import PRODUCTS_LIST_STYLE from "./styled";
 
-const ProductList = ({products, loadSearch}) => {
-	const loader = Array.from({length: 12}, (v, i) => i);
+const ProductList = ({ products, loadSearch }) => {
+  const loader = Array.from({ length: 12 }, (v, i) => i);
 
-	return (
-		<PRODUCTS_LIST_STYLE>
-			<div className='cards__container'>
-				{products ? (
-					products.map(({product: {name, price, imageUrl, _id, combo}}) => (
-						<CardProduct
-							key={_id}
-							name={name}
-							price={price}
-							imageUrl={imageUrl}
-							_id={_id}
-							combo={combo}
-							loading={false}
-						/>
-					))
-				) : loadSearch === true ? (
-					<img
-						className='notFound'
-						src='https://res.cloudinary.com/dlexbrcrv/image/upload/v1622048281/Proyects/notFound_bs1vct.svg'
-					/>
-				) : (
-					loader.map((e, i) => <CardProduct key={i} loading={true} />)
-				)}
-			</div>
-		</PRODUCTS_LIST_STYLE>
-	);
+  return (
+    <PRODUCTS_LIST_STYLE>
+      <div className="cards__container">
+        {products ? (
+          products.map(({ product: { name, price, imageUrl, _id, combo } }) => (
+            <CardProduct
+              key={_id}
+              name={name}
+              price={price}
+              imageUrl={imageUrl}
+              _id={_id}
+              combo={combo}
+              loading={false}
+            />
+          ))
+        ) : loadSearch === true ? (
+          <img
+            className="notFound"
+            src="https://res.cloudinary.com/dlexbrcrv/image/upload/v1622048281/Proyects/notFound_bs1vct.svg"
+            alt=""
+          />
+        ) : (
+          loader.map((e, i) => <CardProduct key={i} loading={true} />)
+        )}
+      </div>
+    </PRODUCTS_LIST_STYLE>
+  );
 };
 
 export default ProductList;
--- client/src/components/containers/productsList/styled.js
+++ client/src/components/containers/productsList/styled.js
@@ -1,19 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Products_List_Style = styled.div`
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 
-	.cards__container {
-		display: flex;
-		justify-content: center;
-		align-content: flex-start;
-		flex-wrap: wrap;
+  .cards__container {
+    display: flex;
+    justify-content: center;
+    align-content: flex-start;
+    margin-top:2.5rem;
+    flex-wrap: wrap;
 
-		.notFound {
-			width: 75%;
-		}
-	}
+    .notFound {
+      width: 75%;
+    }
+  }
 `;
 
 export default Products_List_Style;
